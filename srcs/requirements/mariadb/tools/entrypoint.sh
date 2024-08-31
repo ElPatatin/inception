276cbd539240126b818c8dc0173ee0bdc4b93934
Cristóbal Peset Cruella <43825378+ElPatatin@users.noreply.github.com>
2024-08-31 12:14:54 +0200

    changed the mariadb container configuration due to erros. Also finished the init.sql file and entrypoint.

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,1 +1,45 @@
 #!/bin/bash
+
+# Install MYSQL
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+# Start MYSQL
+mysqld --user=mysql --init-file=/init.sql &
+# /etc/init.d/mysql start
+
+# Create database
+if [ ! -d "/var/lib/mysql${MYSQL_DATABASE}" ]; then
+    mysql_secure_installation <<EOF
+Y
+${MYSQL_ROOT_PASSWORD}
+${MYSQL_ROOT_PASSWORD}
+Y
+n
+Y
+Y
+EOF
+
+    mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+    if [ "${MYSQL_DATABASE}" != "" ]; then
+        mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    fi
+
+    mysql -u root -e "FLUSH PRIVILEGES;"
+
+    if [ -f "/init.sql" ]; then
+        mysql -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /init.sql
+    fi
+
+    echo "Database created"
+else
+    echo "Database already exists"
+fi
+
+# Stop MYSQL
+# killall mysqld
+# sleep 10
+/etc/init.d/mysql stop
+
+exec "$@"
