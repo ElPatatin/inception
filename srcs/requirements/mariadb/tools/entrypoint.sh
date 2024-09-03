713ee6f20962e70d35bf472ad9b08b8ccd4270bf
ElPatatin <cristobalpes1@gmail.com>
2024-09-03 19:27:32 +0200

    i want to kill someone

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
 # Install MYSQL
-mysql_install_db
+# mysql_install_db
 
 # Start MYSQL
-/etc/init.d/mysql start
+
+service mariadb start
+
 
 # Create database
-if [ ! -d "/var/lib/mysql${MYSQL_DATABASE}" ]; then
-    mysql_secure_installation << _EOF_
+mysql_secure_installation << _EOF_
 
-Y
-${MYSQL_ROOT_PASSWORD}
-${MYSQL_ROOT_PASSWORD}
-Y
 n
 Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
 Y
-
+Y
+Y
+Y
 _EOF_
 
-    mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-    if [ "${MYSQL_DATABASE}" != "" ]; then
-        mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    fi
-
-    mysql -u root -e "FLUSH PRIVILEGES;"
-
-    if [ -f "/usr/local/bin/init.sql" ]; then
-        mysql -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < /usr/local/bin/init.sql
-    fi
-
+if [ ! -d "/var/lib/mysql${MYSQL_DATABASE}" ]; then
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;"
+    mysql -e "FLUSH PRIVILEGES;"
     echo "Database created"
 else
     echo "Database already exists"
 fi
 
-/etc/init.d/mysql stop
+service mariadb stopss
 
 exec "$@"
