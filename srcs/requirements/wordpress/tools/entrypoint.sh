a3510f3a2a8deb2d57552aa35c20693ac58c1ea9
ElPatatin <cristobalpes1@gmail.com>
2024-09-04 16:56:24 +0200

    WORDPRESS IS ONLINE BOYS!

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # Set up WordPress if it's not already installed
-if [ -f ./wp-config.php ]; then
-    echo "WordPress already installed"
-else
-    echo "Downloading WordPress..."
+if [ ! -f wp-config.php ]; then
     wp core download --allow-root
-    wp core create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD -dbhost=$DB_HOST --allow-root
+    wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root
     wp core install --url=$DOMAIN_NAME --title="$WORDPRESS_TITL"E --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
     wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
     # wp theme install twentynineteen --activate --allow-root
@@ -14,4 +11,4 @@ else
 fi
 
 # Execute CMD from the Dockerfile
-/usr/bin/php-fpm7.4 -F;
+/usr/sbin/php-fpm7.4 -F;
